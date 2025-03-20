4ec20e9da2505ec462c40cae73dfa1223ebd368a
ZWN410 <162914479+ZWN410@users.noreply.github.com>
2025-03-20 15:51:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+ # Copyright (C) 2024 PokeDoc / NerfDuDu
+ #
+ # Ce programme est un logiciel libre : vous pouvez le redistribuer et/ou le modifier
+ # selon les termes de la licence GNU Lesser General Public License (LGPL) version 3,
+ # telle que publiée par la Free Software Foundation.
+ #
+ # Ce programme est distribué SANS AUCUNE GARANTIE ; sans même la garantie implicite
+ # de COMMERCIALISATION ou D’ADÉQUATION À UN USAGE PARTICULIER.
+ # Voir la licence GNU LGPL v3 pour plus de détails.
+ #
+ # Vous devriez avoir reçu une copie de la licence GNU LGPL v3 avec ce programme.
+ # Sinon, consultez <https://www.gnu.org/licenses/>.
+ 
 setup(
     name="PokeDoc",
     version="1.0",
